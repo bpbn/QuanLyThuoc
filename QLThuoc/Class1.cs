c74937e9658f11657533a62095d0c4797c25737d
WinWantWan <143304302+WinWantWan@users.noreply.github.com>
2023-10-21 15:22:08 +0700

    add class

---
+++ QLThuoc/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLThuoc
+{
+    class Class1
+    {
+        public string ten { get; set; }
+    }
+}
