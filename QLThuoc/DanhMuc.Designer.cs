c6bfff9bcec1d3cba9e0f06d6397ee74efc44780
WinWantWan <143304302+WinWantWan@users.noreply.github.com>
2023-10-22 22:32:27 +0700

    add font

--- QLThuoc/DanhMuc.Designer.cs
+++ QLThuoc/DanhMuc.Designer.cs
@@ -140,7 +140,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
-            this.ClientSize = new System.Drawing.Size(1428, 603);
+            this.ClientSize = new System.Drawing.Size(1428, 713);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
